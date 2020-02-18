ca6df2769ddc6f13b8ad74ca2f96bf84dac9a381
Alexey Shtanko <adamluissean@gmail.com>
2020-02-18 10:21:38 +0200

    Add dependencies

--- src/main/kotlin/Dependencies.kt
+++ src/main/kotlin/Dependencies.kt
@@ -1,3 +1,5 @@
+@file:Suppress("ClassName", "unused")
+
 import Versions.APP_CENTER_VERSION
 import Versions.BATTERY
 import Versions.COIL_VERSION
@@ -23,39 +25,149 @@ import extensions.implementation
 import extensions.kapt
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
+object Versions {
+
+    internal const val KOTLIN_VERSION = "1.3.61"
+    internal const val GRADLE_PLUGIN_VERSION = "4.0.0-alpha09"
+    internal const val DAGGER_VERSION = "2.25.2"
+    internal const val RETROFIT_VERSION = "2.6.2"
+    internal const val GOOGLE_SERVICES_VERSION = "4.0.1"
+    internal const val ANDROID_SUPPORT_VERSION = "1.1.0"
+    internal const val APPCOMPAT_VERSION = "1.1.0-alpha05"
+    internal const val FRAGMENT_VERSION = "1.2.1"
+    internal const val OKHTTP_VERSION = "4.2.2"
+    internal const val LIFECYCLE_VERSION = "2.2.0-alpha03"
+    internal const val COIL_VERSION = "0.7.0"
+    internal const val KOTLIN_COROUTINES_VERSION = "1.3.0"
+    internal const val ROOM_VERSION = "2.0.0"
+    internal const val ZXING_VERSION = "3.3.0"
+    internal const val NAVIGATION_VERSION = "2.1.0"
+    internal const val FACEBOOK_LOGIN_VERSION = "5.5.1"
+
+    internal const val RX_JAVA_2_VERSION = "2.2.14"
+    internal const val RX_JAVA_3_VERSION = "3.0.0-RC4"
+
+    internal const val RX_ANDROID_2_VERSION = "2.1.1"
+    internal const val RX_ANDROID_3_VERSION = "3.0.0-SNAPSHOT"
+
+    internal const val JUNIT_VERSION = "4.12"
+    internal const val MOCKITO_VERSION = "2.18.3"
+    internal const val RUNNER_VERSION = "1.1.1"
+    internal const val FIREBASE_VERSION = "16.0.8"
+    internal const val WORK_VERSION = "2.0.1"
+    internal const val APP_CENTER_VERSION = "1.11.3"
+    internal const val KOIN_VERSION = "2.0.1"
+    internal const val TOOTHPICK_VERSION = "3.1.0"
+    internal const val KODEIN_VERSION = "6.4.0"
+    internal const val ARCH_CORE_VERSION = "2.1.0"
+
+    internal const val PREFERENCE_VERSION  = "1.1.0"
+
+    //region facebook
+    internal const val LITHO_VERSION = "0.33.0"
+    internal const val STETHO_VERSION = "1.5.1"
+    internal const val FLIPPER_VERSION = "0.30.1"
+    internal const val BATTERY = "1.0.0"
+
+    const val DETEKT_VERSION = "1.0.0-RC16"
+    const val KTLINT_VERSION = "8.2.0"
+    const val ERRORPRONE_VERSION = "2.3.3"
+
+    object Android {
+        object Build {
+            const val COMPILE_SDK_VERSION = 29
+            const val MIN_SDK_VERSION = 21
+            const val TARGET_SDK_VERSION = 29
+        }
+    }
+}
+
+
 object Dependencies {
 
-    object App {
+    object Android {
+
         const val GRADLE_PLUGIN = "com.android.tools.build:gradle:$GRADLE_PLUGIN_VERSION"
-        const val GOOGLE_SERVICES = "com.google.gms:google-services:$GOOGLE_SERVICES_VERSION"
-        const val FABRIC = "io.fabric.tools:gradle:1.25.4"
+
+        object AndroidX {
+            const val APPCOMPAT = "androidx.appcompat:appcompat:${Versions.APPCOMPAT_VERSION}"
+            const val CORE  ="androidx.core:core:1.3.0-alpha01"
+            const val CORE_KTX = "androidx.core:core-ktx:1.3.0-alpha01"
+            const val ANNOTATIONS = "androidx.annotation:annotation:1.1.0"
+            const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.0.0-beta4"
+            const val MATERIAL = "com.google.android.material:material:1.2.0-alpha04"
+            const val ACTIVITY_KTX = "androidx.activity:activity-ktx:1.1.0"
+            const val RECYCLER_VIEW = "androidx.recyclerview:recyclerview:1.1.0"
+            const val CARD_VIEW = "androidx.cardview:cardview:1.0.0"
+            const val FRAGMENT_RUNTIME = "androidx.fragment:fragment:${Versions.FRAGMENT_VERSION}"
+            const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT_VERSION}"
+            const val FRAGMENT_TESTING = "androidx.fragment:fragment-testing:${Versions.FRAGMENT_VERSION}"
+            const val NAVIGATION_FRAGMENT = "android.arch.navigation:navigation-fragment:1.0.0"
+            const val NAVIGATION_UI = "android.arch.navigation:navigation-ui:1.0.0"
+            const val CAMERA_CORE = "androidx.camera:camera-core:1.0.0-alpha09"
+            const val CAMERA2 = "androidx.camera:camera-camera2:1.0.0-alpha09"
+            const val CAMERA_LIFECYCLE = "androidx.camera:camera-lifecycle:1.0.0-alpha03"
+            const val CAMERA_VIEW = "androidx.camera:camera-view:1.0.0-alpha06"
+            const val PAGING = "androidx.paging:paging-runtime-ktx:2.1.1"
+            const val ANDROID_SECURITY_CRYPTO = "androidx.security:security-crypto:1.0.0-alpha02"
+            const val CUSTOM_TABS = "androidx.browser:browser:1.3.0-alpha01"
+            const val DRAWER_LAYOUT = ":drawerlayout:1.1.0-alpha03"
+            const val EMOJI = "androidx.emoji:emoji:1.0.0"
+            const val EMOJI_APPCOMPAT = "androidx.emoji:emoji-appcompat:1.0.0"
+            const val VIEW_PAGER_2 = "androidx.viewpager2:viewpager2:1.0.0"
+            const val SWIPE_REFRESH = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0-alpha03"
+            const val PALETTE = "androidx.palette:palette:1.0.0"
+            const val PALETTE_KTX = "androidx.palette:palette-ktx:1.0.0"
+            const val PREFERENCE = "androidx.preference:preference:${Versions.PREFERENCE_VERSION}"
+            const val PREFERENCE_KTX = "androidx.preference:preference-ktx:${Versions.PREFERENCE_VERSION}"
+
+        }
+
+        object Lifecycle {
+            const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata:$LIFECYCLE_VERSION"
+            const val LIFECYCLE_JAVA8 = "androidx.lifecycle:lifecycle-common-java8:$LIFECYCLE_VERSION"
+            const val LIVEDATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:$LIFECYCLE_VERSION"
+            const val EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:$LIFECYCLE_VERSION"
+            const val COMPILER = "androidx.lifecycle:lifecycle-compiler:$LIFECYCLE_VERSION"
+            const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel:$LIFECYCLE_VERSION"
+            const val KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:$LIFECYCLE_VERSION"
+            const val SAVED_STATE = "androidx.lifecycle:lifecycle-viewmodel-savedstate:$LIFECYCLE_VERSION"
+        }
+
+
+        object Room {
+            const val COMPILER = "android.arch.persistence.room:compiler:${Versions.ROOM_VERSION}"
+            const val RUNTIME = "android.arch.persistence.room:runtime:${Versions.ROOM_VERSION}"
+            const val COROUTINES = "androidx.room:room-coroutines:2.1.0-alpha04"
+            const val KTX = "androidx.room:room-ktx:${Versions.ROOM_VERSION}"
+            const val RX_JAVA2 = "androidx.room:room-rxjava2:${Versions.ROOM_VERSION}"
+        }
+
     }
 
-    object AndroidSdk {
-        const val ACTIVITY_KTX = "androidx.activity:activity-ktx:1.1.0"
-        const val APPCOMPAT = "androidx.appcompat:appcompat:${Versions.APPCOMPAT_VERSION}"
-        const val RECYCLER_VIEW = "androidx.recyclerview:recyclerview:1.1.0"
-        const val CARD_VIEW = "androidx.cardview:cardview:1.0.0"
-        const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.0.0-beta4"
-        const val MATERIAL = "com.google.android.material:material:1.2.0-alpha04"
-        const val ANNOTATION = "androidx.annotation:annotation:1.1.0"
-        const val KTX_CORE = "androidx.core:core-ktx:1.3.0-alpha01"
-        const val FRAGMENT_RUNTIME = "androidx.fragment:fragment:${Versions.FRAGMENT_VERSION}"
-        const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT_VERSION}"
-        const val FRAGMENT_TESTING =
-            "androidx.fragment:fragment-testing:${Versions.FRAGMENT_VERSION}"
+    object Kotlin {
 
-        const val NAVIGATION_FRAGMENT = "android.arch.navigation:navigation-fragment:1.0.0"
-        const val NAVIGATION_UI = "android.arch.navigation:navigation-ui:1.0.0"
-        const val CAMERA_CORE = "androidx.camera:camera-core:1.0.0-alpha09"
-        const val CAMERA2 = "androidx.camera:camera-camera2:1.0.0-alpha09"
-        const val CAMERA_LIFECYCLE = "androidx.camera:camera-lifecycle:1.0.0-alpha03"
-        const val CAMERA_VIEW = "androidx.camera:camera-view:1.0.0-alpha06"
+        object Build {
+            const val KOTLIN_GRADLE_PLUGIN =
+                "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
+        }
 
-        const val PAGING = "androidx.paging:paging-runtime-ktx:2.1.1"
-        const val ANDROID_SECURITY_CRYPTO = "androidx.security:security-crypto:1.0.0-alpha02"
+        object Stdlib {
+            const val CORE = "org.jetbrains.kotlin:kotlin-stdlib:$KOTLIN_VERSION"
+        }
 
+        const val STDLIB = "stdlib"
 
+        const val COROUTINES =
+            "org.jetbrains.kotlinx:kotlinx-coroutines-core:$KOTLIN_COROUTINES_VERSION"
+        const val COROUTINES_ANDROID =
+            "org.jetbrains.kotlinx:kotlinx-coroutines-android:$KOTLIN_COROUTINES_VERSION"
+    }
+
+    object App {
+
+        const val GOOGLE_SERVICES = "com.google.gms:google-services:$GOOGLE_SERVICES_VERSION"
+        const val FABRIC = "io.fabric.tools:gradle:1.25.4"
     }
 
     object Other {
@@ -139,24 +251,7 @@ object Dependencies {
         }
     }
 
-    object Kotlin {
 
-        object Build {
-            const val KOTLIN_GRADLE_PLUGIN =
-                "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
-        }
-
-        object Stdlib {
-            const val CORE = "org.jetbrains.kotlin:kotlin-stdlib:$KOTLIN_VERSION"
-        }
-
-        const val STDLIB = "stdlib"
-
-        const val COROUTINES =
-            "org.jetbrains.kotlinx:kotlinx-coroutines-core:$KOTLIN_COROUTINES_VERSION"
-        const val COROUTINES_ANDROID =
-            "org.jetbrains.kotlinx:kotlinx-coroutines-android:$KOTLIN_COROUTINES_VERSION"
-    }
 
     object Injection {
         const val DAGGER = "com.google.dagger:dagger:$DAGGER_VERSION"
@@ -182,6 +277,9 @@ object Dependencies {
         // Koin for Android Architecture ViewModel
         const val KOIN_VIEW_MODEL = "org.koin:koin-android-viewmodel:$KOIN_VERSION"
 
+        //Koin for Android Fragment Factory (unstable version)
+        const val KOIN_FRAGMENT = "org.koin:koin-androidx-fragment:$KOIN_VERSION"
+
         const val TOOTHPICK_RUNTIME =
             "com.github.stephanenicolas.toothpick:toothpick-runtime:$TOOTHPICK_VERSION"
         const val TOOTHPICK_ANDROID =
@@ -191,12 +289,6 @@ object Dependencies {
             "com.github.stephanenicolas.toothpick:toothpick-compiler:$TOOTHPICK_VERSION"
 
         const val KODEIN = "org.kodein.di:kodein-di-generic-jvm:$KODEIN_VERSION"
-    }
-
-    object Room {
-        const val COMPILER = "android.arch.persistence.room:compiler:${Versions.ROOM_VERSION}"
-        const val RUNTIME = "android.arch.persistence.room:runtime:${Versions.ROOM_VERSION}"
-        const val COROUTINES = "androidx.room:room-coroutines:2.1.0-alpha04"
     }
 
     object ArchCore {
@@ -211,6 +303,8 @@ object Dependencies {
         const val RETROFIT_COROUTINES_ADAPTER =
             "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2"
         const val OKHTTP = "com.squareup.okhttp3:okhttp:$OKHTTP_VERSION"
+        const val MOCK_SERVER = "com.squareup.okhttp3:mockwebserver:$OKHTTP_VERSION"
+
         const val LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:$OKHTTP_VERSION"
         const val GSON = "com.google.code.gson:gson:2.8.5"
         const val KLAXON = "com.beust:klaxon:5.0.1"
@@ -233,15 +327,7 @@ object Dependencies {
         const val GLIDE_COMPILER = "com.github.bumptech.glide:compiler:4.10.0"
     }
 
-    object Lifecycle {
-        const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata:$LIFECYCLE_VERSION"
-        const val LIFECYCLE_JAVA8 = "androidx.lifecycle:lifecycle-common-java8:$LIFECYCLE_VERSION"
-        const val LIVEDATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:$LIFECYCLE_VERSION"
-        const val EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:$LIFECYCLE_VERSION"
-        const val COMPILER = "androidx.lifecycle:lifecycle-compiler:$LIFECYCLE_VERSION"
-        const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel:$LIFECYCLE_VERSION"
-        const val KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:$LIFECYCLE_VERSION"
-    }
+
 
     object Firebase {
         const val CORE = "com.google.firebase:firebase-core:16.0.8"
